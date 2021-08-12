9cfc2d3ebdbb463732593105d8e73fe65ae9e0a4
tungngovn <tung.ngothanh.hust@gmail.com>
2021-08-12 15:10:52 +0700

    Edit script to create datasets shortcuts

--- scripts/link_dataset.sh
+++ scripts/link_dataset.sh
@@ -1,4 +1,8 @@
-ln -s /mnt/data/StereoDataset/dataset/kitti2015 ./data
-ln -s /mnt/data/StereoDataset/dataset/kitti2012 ./data
+mkdir -p data/KITTI/kitti2012
+mkdir -p data/KITTI/kitti2015
+mkdir -p data/apolloscape
+
+ln -s /mnt/data/StereoDataset/dataset/kitti2015 ./data/KITTI/kitti2015
+ln -s /mnt/data/StereoDataset/dataset/kitti2012 ./data/KITTI/kitti2012
 ln -s /mnt/data/StereoDataset/dataset/SceneFlow ./data
-ln -s /mnt/data/StereoDataset/dataset/apolloscape ./data
+ln -s /mnt/data/StereoDataset/dataset/apolloscape ./data/apolloscape
