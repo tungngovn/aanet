7569e193dacddc9d466d3533b74d6e8fe30d32ce
Tung Ngo <tung.ngothanh.hust@gmail.com>
2021-08-12 10:32:35 +0200

    Edit for particular dataset links

--- scripts/link_dataset.sh
+++ scripts/link_dataset.sh
@@ -1,8 +1,4 @@
-mkdir -p data/KITTI/kitti2012
-mkdir -p data/KITTI/kitti2015
-mkdir -p data/apolloscape
-
-ln -s /mnt/data/StereoDataset/dataset/kitti2015 ./data/KITTI/kitti2015
-ln -s /mnt/data/StereoDataset/dataset/kitti2012 ./data/KITTI/kitti2012
+ln -s /home/hanguyen/dataset/KITTI2015/ ./data/KITTI/
+ln -s /home/hanguyen/dataset/KITTI2012/ ./data/KITTI/
 ln -s /mnt/data/StereoDataset/dataset/SceneFlow ./data
-ln -s /mnt/data/StereoDataset/dataset/apolloscape ./data/apolloscape
+ln -s /home/hanguyen/dataset/apolloscape/ ./data/
