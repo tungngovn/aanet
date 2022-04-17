d506e5e3e34f2ca75db19e924531d731de9eddcc
tungngovn <tung.ngothanh.hust@gmail.com>
2022-04-17 14:41:57 +0700

    KITTI2012 evaluation

--- scripts/link_dataset.sh
+++ scripts/link_dataset.sh
@@ -1,4 +1,4 @@
 #ln -s /home/hanguyen/dataset/KITTI2015/ ./data/KITTI/
-#ln -s /home/hanguyen/dataset/KITTI2012/ ./data/KITTI/
+ln -s /home/hanguyen/dataset/KITTI2012/ ./data/KITTI/
 #ln -s /mnt/data/StereoDataset/dataset/SceneFlow ./data
 ln -s /home/hanguyen/dataset/apolloscape/ ./data/
