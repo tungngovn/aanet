4c1cf53cdef85a7dc3ad35dae606b64b5895c5f2
tungngovn <tung.ngothanh.hust@gmail.com>
2022-05-23 22:48:21 +0700

    debug

--- inference_crop.py
+++ inference_crop.py
@@ -14,6 +14,7 @@ from dataloader import transforms
 from utils import utils
 from utils.file_io import write_pfm
 
+import pdb
 from PIL import Image, ImageDraw
 from metric import d1_metric, thres_metric, dist_err
 
@@ -194,6 +195,8 @@ def main():
 
         print("No. of bbox: ", len(sample['left_bboxes']))
         if len(sample['left_bboxes']) == 0: continue
+        if len(sample['left_bboxes']) == 1: 
+            pdb.set_trace()
         for j, bbox in enumerate(sample['left_bboxes']):
             ## bbox: [<class>, <x_min>, <y_min>, <x_max>, <y_max>]
 
