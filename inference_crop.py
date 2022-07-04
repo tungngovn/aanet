2e56534636944c876854ecf971938cae15c8af9c
tungngovn <tung.ngothanh.hust@gmail.com>
2022-07-04 21:57:39 +0700

    add pdb to debug

--- inference_crop.py
+++ inference_crop.py
@@ -357,6 +357,7 @@ def main():
                         left_imge.save(save_name_left)
 
                 ### Distance error
+                pdb.set_trace()
                 dist_error = dist_err((disp_pred * 256.).astype(np.uint16), (disp_gt* 256.).astype(np.uint16), mask.detach().cpu().numpy())
                 # dist_errs += dist_error*(x_max - x_min_bb)*(y_max-y_min_bb)
                 dist_errs += dist_error
