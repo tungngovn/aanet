94e716c7c8179321c8770fa5e37d03fd6e56cf35
tungngovn <tung.ngothanh.hust@gmail.com>
2022-07-08 23:20:32 +0700

    edit size training

--- scripts/aanet+_finetune_apolloscape.sh
+++ scripts/aanet+_finetune_apolloscape.sh
@@ -5,14 +5,14 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py \
 --mode val \
 --data_dir data/apolloscape \
 --dataset_name apolloscape \
---checkpoint_dir checkpoints/apolloscape_2nd \
+--checkpoint_dir checkpoints/apolloscape_2nd_2nd \
 --pretrained_aanet pretrained/aanet+_sceneflow-d3e13ef0.pth \
---batch_size 8 \
+--batch_size 4 \
 --val_batch_size 1 \
---img_height 384 \
---img_width 1248 \
---val_img_height 384 \
---val_img_width 1248 \
+--img_height 288 \
+--img_width 576 \
+--val_img_height 288 \
+--val_img_width 576 \
 --max_disp 192 \
 --feature_type ganet \
 --feature_pyramid \
